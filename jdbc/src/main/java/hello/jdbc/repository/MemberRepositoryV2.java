824d0947a300106e88a54673f21b7f38fe6a4a53
ks12b0000 <ks12b0000@gmail.com>
2022-11-05 21:42:18 +0900

    2022-11-05

--- jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV2.java
+++ jdbc/src/main/java/hello/jdbc/repository/MemberRepositoryV2.java
@@ -9,14 +9,14 @@ import java.sql.*;
 import java.util.NoSuchElementException;
 
 /*
-*  JDBC - DataSource 사용, JDBCUtils 사용
+*  JDBC - ConnectionParam
 * */
 @Slf4j
-public class MemberRepositoryV1 {
+public class MemberRepositoryV2 {
 
     private final DataSource dataSource;
 
-    public MemberRepositoryV1(DataSource dataSource) {
+    public MemberRepositoryV2(DataSource dataSource) {
         this.dataSource = dataSource;
     }
 
@@ -45,15 +45,15 @@ public class MemberRepositoryV1 {
         String sql = "select * from member where member_id = ?";
 
         Connection con = null;
-        PreparedStatement psmt = null;
+        PreparedStatement pstmt = null;
         ResultSet rs = null;
 
         try {
             con = getConnection();
-            psmt = con.prepareStatement(sql);
-            psmt.setString(1, memberId);
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, memberId);
 
-            rs = psmt.executeQuery();
+            rs = pstmt.executeQuery();
             if (rs.next()) {
                 Member member = new Member();
                 member.setMemberId(rs.getString("member_id"));
@@ -67,7 +67,37 @@ public class MemberRepositoryV1 {
             log.error("db error", e);
             throw e;
         } finally {
-            close(con, psmt, rs);
+            close(con, pstmt, rs);
+        }
+    }
+
+    public Member findById(Connection con, String memberId) throws SQLException {
+        String sql = "select * from member where member_id = ?";
+
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, memberId);
+
+            rs = pstmt.executeQuery();
+            if (rs.next()) {
+                Member member = new Member();
+                member.setMemberId(rs.getString("member_id"));
+                member.setMoney(rs.getInt("money"));
+                return member;
+            } else {
+                throw new NoSuchElementException("member not found memberId = " + memberId);
+            }
+
+        } catch (SQLException e) {
+            log.error("db error", e);
+            throw e;
+        } finally {
+            // connection은 여기서 닫지 않음.
+            JdbcUtils.closeResultSet(rs);
+            JdbcUtils.closeStatement(pstmt);
         }
     }
 
@@ -89,6 +119,26 @@ public class MemberRepositoryV1 {
             throw e;
         } finally {
             close(con, psmt, null);
+        }
+    }
+
+    public void update(Connection con, String memberId, int money) throws SQLException {
+        String sql = "update member set money = ? where member_id = ?";
+
+        PreparedStatement pstmt = null;
+
+        try {
+            pstmt = con.prepareStatement(sql);
+            pstmt.setInt(1, money);
+            pstmt.setString(2, memberId);
+            int resultSize = pstmt.executeUpdate();
+            log.info("resultSize = {}", resultSize);
+        } catch (SQLException e) {
+            log.error("db error", e);
+            throw e;
+        } finally {
+            // connection은 여기서 닫지 않음.
+            JdbcUtils.closeStatement(pstmt);
         }
     }
 
--- jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV1Test.java
+++ jdbc/src/test/java/hello/jdbc/repository/MemberRepositoryV1Test.java
@@ -1,12 +1,10 @@
 package hello.jdbc.repository;
 
 import com.zaxxer.hikari.HikariDataSource;
-import hello.jdbc.connection.ConnectionConst;
 import hello.jdbc.domain.Member;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import java.sql.SQLException;
 import java.util.NoSuchElementException;
@@ -18,7 +16,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 @Slf4j
 class MemberRepositoryV1Test {
 
-    MemberRepositoryV1 repository;
+    MemberRepositoryV2 repository;
 
     @BeforeEach
     void beforeEach() {
@@ -31,7 +29,7 @@ class MemberRepositoryV1Test {
         dataSource.setUsername(USERNAME);
         dataSource.setPassword(PASSWORD);
 
-        repository = new MemberRepositoryV1(dataSource);
+        repository = new MemberRepositoryV2(dataSource);
     }
 
     @Test
